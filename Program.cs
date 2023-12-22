c8711ffd18399ab5ba4ca3aca583764c6d1a3a44
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2023-12-22 09:22:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__OOP_HW008_SnowBalls_r00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
